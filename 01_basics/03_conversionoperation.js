5496b9ce37fd333907914895d5f80a1616bbb7ad
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-03-24 14:53:48 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+//conversion operation 
+
+//String to number conversion 
+let score="14";
+
+console.log(typeof score );
+console.log(typeof(score));
+
+let ValueInNumber= Number(score);
+console.log( typeof ValueInNumber );
+
+//"333"=333
+//"333abc"=nan not a number
+
+let Islogin = 1 
+
+let booleanIsloggedin=Boolean(Islogin);
+console.log(booleanIsloggedin);
+
+// 1= true 2 = false
+
