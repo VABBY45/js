bcf3013b18c40ef282e72e721fd5c09da989a4cc
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-03-24 14:39:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+//nuber
+//big int 
+//string
+//boolean
+//null
+//undefined
+//object
+
+const accountid=14;
+
+console.log(typeof accountid);
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountid=14;
+let accountemail="vaibhavpatil1145@gmail.com";
+var accountpass=1223;
+
+console.log(accountid);
+
