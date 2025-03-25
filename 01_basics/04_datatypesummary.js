6bb4a0de805ac432794ad46931161de2ea6ae8c8
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-03-25 13:48:57 +0000

    datatype summary

---
+++ 01_basics/04_datatypesummary.js
@@ -0,0 +1,20 @@
+// primitive datatype
+//String,number,boolean,null,undefined,symbol
+//non-primitive datatype 
+// Array,object and function 
+
+ const car=["renault","ford","Kawasaki"];
+
+ let myobj={
+    name:vaibhav,
+    Age:12
+ }
+
+ const myfunc=function(){
+console.log("hellow world");
+
+ } 
+ console.log(typeof myfunc);
+ 
+ 
+
