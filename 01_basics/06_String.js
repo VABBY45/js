706188ab3ca73252132ff355876b232df588848c
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-03-27 12:53:29 +0000

    Strings

---
+++ 01_basics/06_String.js
@@ -0,0 +1,28 @@
+const name = "vaibhav";
+const repoCount = 50;
+
+//concatenate 
+
+console.log(`my name is ${name} and my repoCount is ${repoCount}`);
+
+const myName = new String ("vaibhav-patil-com")
+
+//chatAt=in which position which character is available 
+console.log(name.charAt(2))
+
+//split= we can  split string using specific symbol 
+
+console.log(myName.split("-"));
+
+const url = "https://vaibhav.com/vaibhav%20patil"
+
+//replace = we can replace %20 to which symbol we want 
+
+console.log(url.replace('%20' , '-'));
+
+
+
+
+
+
+
