a7b985432704b29782494f48eefa006d01801780
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-04-21 12:12:41 +0000

    math and num

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,26 @@
+const score =400;
+console.log(score);
+const balance=new Number(100);
+console.log(balance);
+// in java script we can also change number to string  here is convesion .
+
+console.log(balance.toString().length)
+//too fixed-- it commonly use in E-commerecde web site.
+console.log(balance.toFixed(2));
+//precision
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+//++++++++++++++++++++++++Math++++++++++++++++++++++++
+
+
+console.log(Math)
+console.log(Math.sqrt(25));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10+1));
+
+const max=5;
+const min=1;
+
+console.log(Math.floor(Math.random() * (max-min+1)+min));
