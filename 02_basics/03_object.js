7af07cee1a81ca00bcc77947ec647562ad59ffb4
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-04-22 12:54:09 +0000

    objects

---
+++ 02_basics/03_object.js
@@ -0,0 +1,39 @@
+//what is singleton-
+//learn object declaration 
+
+const mysym=Symbol("key1");
+const jsuser={
+
+    name:"vaibhav",
+    mysym:mysym,
+    Age: 24,
+    email:"vaibhavpatil09112001@gmail.com",
+    islogin:"false",
+    lastlogin:["monday","sunday"]
+};
+
+console.log(jsuser.mysym);
+console.log(jsuser.name);
+
+// how to change value 
+jsuser.email="vaibhavpatil09112001@amezon.com" ;
+console.log(jsuser.email);
+
+// how to freez object values
+
+//Object.freeze(jsuser);  // now here are freez the valUE OF OBJECT \\
+ jsuser.name="yash patil";
+ console.log(jsuser.name);  // if we run this code still its no gona change \\
+
+ // now here are code  how to declare function //
+
+ jsuser.greetings=function(){
+    console.log("Hellow js user," );
+ }
+ console.log(jsuser.greetings())
+
+ jsuser.greetings2=function(){
+    console.log(`Hellow js user, ${this.name}`)
+ }
+ console.log(jsuser.greetings2());
+ 
