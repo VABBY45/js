63ebd5bb2adbf2d9e5ee56cdfc63bf8b3e314922
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-04-23 13:11:15 +0000

    param

---
+++ 03_basics/01_FunctionandParameter.js
@@ -0,0 +1,11 @@
+/*here is function ::   now we are going to learn how can we  use function and parameter*/
+function sayMyNane() {
+console.log("V");
+console.log("A");
+console.log("I");
+console.log("B");
+console.log("H");
+console.log("A");
+console.log("V");
+}
+sayMyNane();
