5d47bb24fbe8a53ebeb86867b0d1bc4896f81edf
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-04-25 11:24:26 +0000

    function

--- 03_basics/01_FunctionandParameter.js
+++ 03_basics/01_FunctionandParameter.js
@@ -9,3 +9,20 @@ console.log("A");
 console.log("V");
 }
 sayMyNane();
+
+//Addition without passing parameter //
+function addNumSub(num4,num3) { 
+    console.log(num4+num3)
+}
+
+console.log(addNumSub(4,5));
+
+
+//Addition passsing by parameter //
+function addNum (num1,num2) {
+    
+     let Ans1=num1+num2;   
+     return Ans1;
+}
+let Ans1=addNum(4,8);
+console.log(Ans1);
