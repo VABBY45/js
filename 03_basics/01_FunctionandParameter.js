5d59779c82b4a80f36b324baeb968758de6a5919
VABBY45 <152503925+VABBY45@users.noreply.github.com>
2025-04-25 12:10:03 +0000

    function

--- 03_basics/01_FunctionandParameter.js
+++ 03_basics/01_FunctionandParameter.js
@@ -26,3 +26,48 @@ function addNum (num1,num2) {
 }
 let Ans1=addNum(4,8);
 console.log(Ans1);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+// it is spread operator that is used for collect multipale argument and print in array format 
+function bucketAddList(...obj) {
+    return obj;
+    
+}
+console.log(bucketAddList(44,55,44,77,88,22));
+
+//here is an exaple of object how to pass in function using return //
+
+const user={
+    name:"vaibhav",
+    lastname:"patil"
+}
+
+function passingObject(human) {
+    return human;
+} 
+
+console.log(passingObject(user))
+
+
+//here is an exaple of object how to pass in function using console.log //
+
+
+const user2={
+    name:"vaibhav",
+    lastname:"patil"
+}
+
+function passingObject(human2) {
+    console.log(`my last name is ${human2.lastname} ans first name is ${human2.name}`)
+} 
+passingObject(user2);
+
+//now we are going to add  array in function //
+
+const myArray=[55,22,33,44,66,99,88,77,1,2,0,5,8,93,4,8520,741,85296,7415]
+
+function mynewarrayfunction(bamboo) {
+    return bamboo[5];
+}
+console.log(mynewarrayfunction(myArray))
